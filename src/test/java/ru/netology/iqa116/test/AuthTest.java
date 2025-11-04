eb2593475fa56882d155a78fd622d16b6050ab94
Yuriy Volodin <Goliakywillalive@yandex.ru>
2025-11-04 19:35:20 +0500

    Hotfix tests and add pos test.md

--- src/test/java/ru/netology/iqa116/test/AuthTest.java
+++ src/test/java/ru/netology/iqa116/test/AuthTest.java
@@ -2,6 +2,7 @@ package ru.netology.iqa116.test;
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.netology.iqa116.data.DataGenerator;
@@ -16,16 +17,25 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class AuthTest {
 
-    @BeforeAll
-    static void waitForServer() throws InterruptedException {
-        Thread.sleep(5000);
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successfully login with registered active user")
+    void shouldLoginSuccessfully() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id=login] input").setValue(registeredUser.getLogin());
+        $("[data-test-id=password] input").setValue(registeredUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id=error-notification]").shouldNotBe(Condition.visible, Duration.ofSeconds(10));
     }
 
     @Test
     @DisplayName("Should get error if not registered user tries to login")
     void shouldGetErrorIfNotRegisteredUser() {
         var notRegisteredUser = DataGenerator.Registration.getUser("active");
-        open("http://localhost:9999");
         $("[data-test-id=login] input").setValue(notRegisteredUser.getLogin());
         $("[data-test-id=password] input").setValue(notRegisteredUser.getPassword());
         $("button.button").click();
@@ -39,7 +49,6 @@ public class AuthTest {
     @DisplayName("Should get error message if login with blocked registered user")
     void shouldGetErrorIfBlockedUser() {
         var blockedUser = getRegisteredUser("blocked");
-        open("http://localhost:9999");
         $("[data-test-id=login] input").setValue(blockedUser.getLogin());
         $("[data-test-id=password] input").setValue(blockedUser.getPassword());
         $("button.button").click();
@@ -54,7 +63,6 @@ public class AuthTest {
     void shouldGetErrorIfWrongLogin() {
         var registeredUser = getRegisteredUser("active");
         var wrongLogin = getRandomLogin();
-        open("http://localhost:9999");
         $("[data-test-id=login] input").setValue(wrongLogin);
         $("[data-test-id=password] input").setValue(registeredUser.getPassword());
         $("button.button").click();
@@ -69,7 +77,6 @@ public class AuthTest {
     void shouldGetErrorIfWrongPassword() {
         var registeredUser = getRegisteredUser("active");
         var wrongPassword = getRandomPassword();
-        open("http://localhost:9999");
         $("[data-test-id=login] input").setValue(registeredUser.getLogin());
         $("[data-test-id=password] input").setValue(wrongPassword);
         $("button.button").click();
